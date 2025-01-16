f89746a792b7fcb83e844699f2991caf54067e13
MASOUMA2023 <ahmadimasooma12@gmail.com>
2025-01-16 16:41:49 -0500

    answers for lesson 9

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
 const app = express();
+const authRouter = require('./routes/auth')
+const jobsRouter = require('./routes/jobs')
+
 
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
@@ -9,11 +12,17 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(express.json());
 // extra packages
-
+const connectDB = require('./db/connect')
+const authenticateUser = require('./middleware/authentication')
 // routes
 app.get('/', (req, res) => {
   res.send('jobs api');
 });
+
+const apiRouter = express.Router();
+app.use('/api/v1', apiRouter)
+apiRouter.use('/auth', authRouter)
+apiRouter.use('/jobs', authenticateUser, jobsRouter)
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -21,7 +30,9 @@ app.use(errorHandlerMiddleware);
 const port = process.env.PORT || 3000;
 
 const start = async () => {
+
   try {
+    await connectDB(process.env.MONGO_URI)
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
